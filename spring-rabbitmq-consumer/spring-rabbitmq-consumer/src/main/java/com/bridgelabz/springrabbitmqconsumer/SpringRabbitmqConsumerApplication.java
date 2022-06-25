4e1714314d4cb81b81555504f5070581ff7c016d
Shubham <ssp671995@gmail.com>
2022-06-25 11:19:53 +0530

    Added the producer and consumer in the program.

---
+++ spring-rabbitmq-consumer/spring-rabbitmq-consumer/src/main/java/com/bridgelabz/springrabbitmqconsumer/SpringRabbitmqConsumerApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.springrabbitmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqConsumerApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
---
+++ spring-rabbitmq-producer/spring-rabbitmq-producer/src/main/java/com/bridgelabz/springrabbitmqproducer/SpringRabbitmqProducerApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.springrabbitmqproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRabbitmqProducerApplication.class, args);
+        System.out.println("Hello");
+    }
+
+}
